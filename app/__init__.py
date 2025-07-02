19f17b6e6a4ac0467546a129d0a8a61883294a50
J.A.R.V.I.K <jiri.cechura@gmail.com>
2025-07-02 09:17:21 +0200

    Update package docstring

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
-"""Lightweight Flask proxy for KOSTAL GPT.
-Contains main, api_client, memory and knowledge modules."""
+"""Lightweight Flask proxy to access KOSTAL GPT.
+Includes the main, api_client, memory and knowledge modules."""
